e19c7db751335985634c2dd5f706fb1c6d7e0983
Dshlokk <divyashlokk@gmail.com>
2026-05-12 18:28:31 +0530

    Vercel changes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,7 @@
 import server from "../dist/server/server.js";
 
-export const config = {
-  runtime: "edge",
-};
-
 export default async function handler(request) {
   // TanStack Start's server.fetch expects (request, env, context)
-  // On Vercel Edge, we only have the request. We pass empty objects for the rest.
+  // Vercel Node.js functions support the Web Request API.
   return server.fetch(request, {}, {});
 }
